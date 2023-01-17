ae36af069d2ee9111d5169ecc8209aead293d6ac
anetaplzakova <93376846+anetaplzakova@users.noreply.github.com>
2023-01-17 11:11:13 +0100

    Update publishconf.py

--- publishconf.py
+++ publishconf.py
@@ -7,7 +7,10 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://michalfrankl.github.io/ehri-nn-static-czech'
+#SITEURL = 'https://michalfrankl.github.io/ehri-nn-static-czech'
+#RELATIVE_URLS = False
+
+SITEURL = 'https://anetaplzakova.github.io/ehri-nn-static-czech'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
